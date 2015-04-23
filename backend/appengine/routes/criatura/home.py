69de2d3dbf5ea4bf9e50993a6fb4ae1d4913e6ed
marcelosandoval <marceloa.sandoval@gmail.com>
2015-04-22 21:09:10 -0300

    login funfa, tem que ajustar user,  do crud ja ta feito o create, proxima video =watch?v=LJsY_h5l71c&list=PLA05yVJtRWYRGIeBxag8uT-3ftcMVT5oF&index=28

--- backend/appengine/routes/criatura/home.py
+++ backend/appengine/routes/criatura/home.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
-
+from tekton import router
 
 @login_not_required
 @no_csrf
 def index():
     return TemplateResponse()
+
 
--- backend/appengine/routes/criatura/show.py
+++ backend/appengine/routes/criatura/show.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
+from gaeforms import base
+from gaeforms.base import Form
+from gaegraph.model import Node
 from gaepermission.decorator import login_not_required
+from tekton import router
 
 
 @login_not_required
@@ -10,3 +15,65 @@ from gaepermission.decorator import login_not_required
 def index(nome='none'):
     return TemplateResponse()
 
+
+
+@no_csrf
+def form():
+    contexto={'salvar_path':router.to_path(salvar)}
+    return TemplateResponse(contexto,template_path='criatura/form.html')
+
+class Criatura(Node):
+    name=ndb.StringProperty(required=True)
+    type=ndb.StringProperty()
+    gold=ndb.IntegerProperty()
+    xp=ndb.IntegerProperty()
+    hp=ndb.IntegerProperty()
+    damage=ndb.IntegerProperty()
+    atksp=ndb.IntegerProperty()
+    armor=ndb.IntegerProperty()
+    magicres=ndb.IntegerProperty()
+    movesp=ndb.IntegerProperty()
+    spawn=ndb.StringProperty()
+    respawn=ndb.StringProperty()
+    buff=ndb.TextProperty()
+
+class CriaturaForm(Form):
+
+    name=base.StringField(required=True)
+    type=base.StringField()
+    gold=base.IntegerField()
+    xp=base.IntegerField()
+    hp=base.IntegerField()
+    damage=base.IntegerField()
+    atksp=base.IntegerField()
+    armor=base.IntegerField()
+    magicres=base.IntegerField()
+    movesp=base.IntegerField()
+    spawn=base.StringField()
+    respawn=base.StringField()
+    buff=base.StringField()
+
+def salvar(_resp,**prop):
+
+    criaturaF=CriaturaForm(**prop)
+    erros=criaturaF.validate()
+    if erros:
+        _resp.write(erros)
+    else:
+        pass
+
+    #criatura=Criatura(name=prop['name'],
+    #                  type=prop['type'],
+     #                 gold=int(prop['gold']),
+      #                xp=int(prop['xp']),
+       #               hp=int(prop['hp']),
+        #              damage=int(prop['damage']),
+         #             atksp=int(prop['atksp']),
+          #            armor=int(prop['armor']),
+           #           magicres=int(prop['magicres']),
+            #          movesp=int(prop['movesp']),
+             #         spawn=prop['spawn'],
+              #        respawn=prop['respawn'],
+               #       buff=prop['buff'])
+ #   criatura.put()
+  #  _resp.write(prop)
